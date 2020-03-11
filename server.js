1ec72e35bee55912dc0bc14214ca41f48184714c
Mahir Erden <mahirerden@gmail.com>
2020-03-11 10:13:19 -0400

    deploy heroku

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout'
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useFindAndModify: false
 })
 
-require('.routes/apiRoutes')(app)
-require('.routes/htmlRoutes')(app)
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.removeListener(PORT, () => {
-  console.log(`App listening on Port ${PORT}!`)
-})
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
+});
